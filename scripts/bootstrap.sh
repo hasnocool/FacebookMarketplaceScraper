7776c7d92685beafb24aee3ad910d15819e47340
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-08 06:12:59 -0700

    Add bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# scripts/bootstrap.sh
+set -euo pipefail
+
+python3.12 -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+pip install -e '.[dev]'
+playwright install chromium
